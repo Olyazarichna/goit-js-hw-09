474ac0927d8e3744b358f7742d17d15b17ed2f28
Olyazarichna <olya.zarichna@gmail.com>
2022-05-12 11:09:04 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(getRandomHexColor());
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+refs.stop.setAttribute('disabled', 'disabled');
+
+refs.start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.setAttribute('disabled', 'disabled');
+  refs.stop.removeAttribute('disabled', 'disabled');
+});
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.start.removeAttribute('disabled', 'disabled');
+  refs.stop.setAttribute('disabled', 'disabled');
+});
