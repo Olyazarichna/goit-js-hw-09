92712cb2aee37c7109f7d0fc322bec4441a24ece
Olyazarichna <olya.zarichna@gmail.com>
2022-05-15 22:36:43 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,48 +4,42 @@ const formEl = document.querySelector('.form');
 console.log(formEl);
 formEl.addEventListener('submit', onSubmitForm);
 
-
-function onSubmitForm(event){
+function onSubmitForm(event) {
   event.preventDefault();
-
+  // formEl.textContent = '';
   const obj = {
-    delay: formEl.delay.value,
-    step: formEl.step.value,
-    amount: formEl.amount.value,
+    delay: Number(formEl.delay.value),
+    step: Number(formEl.step.value),
+    amount: Number(formEl.amount.value),
+  };
+
+  let delayStep = obj.delay;
+
+  for (let i = 1; i <= obj.amount; i += 1) {
+    createPromise(i, delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayStep += obj.step;
   }
-
-
-  for(let i = 1; i <= obj.amount; i += 1) {
-      createPromise(i, obj.delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-
 }
-  }
-   
-
-
-
 
 function createPromise(position, delay) {
-
-return new Promise(function(resolve, reject) {
-   const shouldResolve = Math.random() > 0.3;
-
-
-  if (shouldResolve) {
-   resolve({position, delay});
-    // Fulfill
-  } else {
-   reject({position, delay});
-    // Reject
-  } 
-})
-
+  return new Promise(function (resolve, reject) {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        reject({ position, delay });
+        // Reject
+      }
+    }, delay);
+  });
 }
-
