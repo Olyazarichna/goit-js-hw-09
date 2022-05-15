3d63210709cdc660fa109e00adf2535d4e156339
Olyazarichna <olya.zarichna@gmail.com>
2022-05-15 22:07:50 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+const formEl = document.querySelector('.form');
+console.log(formEl);
+formEl.addEventListener('submit', onSubmitForm);
+
+
+function onSubmitForm(event){
+  event.preventDefault();
+
+  const obj = {
+    delay: formEl.delay.value,
+    step: formEl.step.value,
+    amount: formEl.amount.value,
+  }
+
+
+  for(let i = 1; i <= obj.amount; i += 1) {
+      createPromise(i, obj.delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+}
+  }
+   
+
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+
+return new Promise(function(resolve, reject) {
+   const shouldResolve = Math.random() > 0.3;
+
+
   if (shouldResolve) {
+   resolve({position, delay});
     // Fulfill
   } else {
+   reject({position, delay});
     // Reject
-  }
+  } 
+})
+
 }
+
